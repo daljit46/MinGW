c3150bb29f93a7a022d42a0f5e65664fd9e867b9
Daljit Singh <daljit7991@gmail.com>
2024-02-23 20:33:55 +0000

    Update for CMake build

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [[ $# -ne 2 ]]; then
 else
 
   set -e
-  sed -i'.orig' -e "s|TAGNAME|$1|g" -e "s|GIT_USER|$2|g" PKGBUILD
+  sed -i'.orig' -e "s|GIT_COMMIT|$1|g" -e "s|GIT_USER|$2|g" PKGBUILD
   makepkg -s
 
 fi
